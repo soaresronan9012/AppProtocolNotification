f35f383207099e9c0b63bd02c68e0ef76f4a0747
Ronan Soares <soaresronansilva@gmail.com>
2024-10-16 21:19:54 -0300

    starting

---
+++ AppProtocolNotificationTests/AppProtocolNotificationTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppProtocolNotificationTests.swift
+//  AppProtocolNotificationTests
+//
+//  Created by Ronan Soares on 16/10/24.
+//
+
+import Testing
+@testable import AppProtocolNotification
+
+struct AppProtocolNotificationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
