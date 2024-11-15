0ef1dbd6f3f75f63e4f519debd763d9183725fee
Ronan Soares <soaresronansilva@gmail.com>
2024-11-14 21:07:15 -0300

    criacao da tela de more informations

---
+++ AppProtocolNotification/Information/InformationScreen.swift
@@ -0,0 +1,20 @@
+//
+//  InformationScreen.swift
+//  AppProtocolNotification
+//
+//  Created by Ronan Soares on 14/11/24.
+//
+
+import UIKit
+
+class InformationScreen: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
