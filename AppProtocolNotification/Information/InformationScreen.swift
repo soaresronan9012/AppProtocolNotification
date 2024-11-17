f550bbfe5416e40245aa99d7dded5e5f46b8418b
Ronan Soares <soaresronansilva@gmail.com>
2024-11-16 22:01:55 -0300

    configurando informationScreen

--- AppProtocolNotification/Information/InformationScreen.swift
+++ AppProtocolNotification/Information/InformationScreen.swift
@@ -11,13 +11,23 @@ class InformationScreen: UIView {
     
     lazy var titleLabel: UILabel = {
         let label = UILabel()
-        label.text = "Welcome!"
+        label.text = "Hello!"
         label.translatesAutoresizingMaskIntoConstraints = false
         label.textColor = .black
         label.textAlignment = .center
         label.font = UIFont.systemFont(ofSize: 50, weight: .bold)
         label.numberOfLines = 0
         return label
+    }()
+    
+    lazy var imageHead : UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "welcome-svgrepo-com")
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.contentMode = .scaleAspectFit
+        image.clipsToBounds = true
+        image.layer.cornerRadius = 45
+        return image
     }()
 
     override init(frame: CGRect) {
@@ -35,6 +45,7 @@ class InformationScreen: UIView {
     
     func addElement() {
         addSubview(titleLabel)
+        addSubview(imageHead)
         
     }
     
@@ -42,8 +53,14 @@ class InformationScreen: UIView {
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 50),
             //titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
-            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor,constant: 30),
-            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor,constant: -30)
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor,constant: 10),
+            //titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor,constant: -30),
+            
+            imageHead.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 30),
+            imageHead.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            //imageHead.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -30),
+            imageHead.heightAnchor.constraint(equalToConstant:70),
+            imageHead.widthAnchor.constraint(equalToConstant: 150),
             ])
     }
     
