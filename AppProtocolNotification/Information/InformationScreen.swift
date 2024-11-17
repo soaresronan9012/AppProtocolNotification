901bd2fd9addca0f0d0bd6c2423c2a39b1bcbf17
Ronan Soares <soaresronansilva@gmail.com>
2024-11-17 20:32:57 -0300

    image para informationScreen

--- AppProtocolNotification/Information/InformationScreen.swift
+++ AppProtocolNotification/Information/InformationScreen.swift
@@ -22,11 +22,11 @@ class InformationScreen: UIView {
     
     lazy var imageHead : UIImageView = {
         let image = UIImageView()
-        image.image = UIImage(named: "welcome-svgrepo-com")
+        image.image = UIImage(named: "pexels-harold-vasquez-853421-2653362")
         image.translatesAutoresizingMaskIntoConstraints = false
-        image.contentMode = .scaleAspectFit
+        //image.contentMode = .scaleAspectFit  // habilita se a imagem ira distorcer ou não
         image.clipsToBounds = true
-        image.layer.cornerRadius = 45
+        image.layer.cornerRadius = 25 // bordas arreondadas
         return image
     }()
 
@@ -53,14 +53,15 @@ class InformationScreen: UIView {
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 50),
             //titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
-            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor,constant: 10),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor,constant: 20),
             //titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor,constant: -30),
             
             imageHead.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 30),
-            imageHead.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
-            //imageHead.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -30),
-            imageHead.heightAnchor.constraint(equalToConstant:70),
-            imageHead.widthAnchor.constraint(equalToConstant: 150),
+            imageHead.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            imageHead.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            //imageHead.centerXAnchor.constraint(equalTo: centerXAnchor),
+            imageHead.heightAnchor.constraint(equalToConstant:150),
+            imageHead.widthAnchor.constraint(equalToConstant: 750),
             ])
     }
     
