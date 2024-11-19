87e6086c8fbc7fe56a7e4f29895c71aaedfa1b24
Ronan Soares <soaresronansilva@gmail.com>
2024-11-18 22:21:55 -0300

    config informationScreen

--- AppProtocolNotification/Information/InformationScreen.swift
+++ AppProtocolNotification/Information/InformationScreen.swift
@@ -11,7 +11,7 @@ class InformationScreen: UIView {
     
     lazy var titleLabel: UILabel = {
         let label = UILabel()
-        label.text = "Hello!"
+        label.text = "Hello dev!"
         label.translatesAutoresizingMaskIntoConstraints = false
         label.textColor = .black
         label.textAlignment = .center
@@ -26,10 +26,22 @@ class InformationScreen: UIView {
         image.translatesAutoresizingMaskIntoConstraints = false
         //image.contentMode = .scaleAspectFit  // habilita se a imagem ira distorcer ou não
         image.clipsToBounds = true
-        image.layer.cornerRadius = 25 // bordas arreondadas
+        image.layer.cornerRadius = 20 // bordas arreondadas
         return image
     }()
-
+    
+    lazy var textLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Welcome to the information page about the Apple information system! Here you can find out about the main topics of the moment in this programming universe."
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    
     override init(frame: CGRect) {
          super.init(frame: frame)
         backgroundColor = .systemGray5
@@ -46,6 +58,7 @@ class InformationScreen: UIView {
     func addElement() {
         addSubview(titleLabel)
         addSubview(imageHead)
+        addSubview(textLabel)
         
     }
     
@@ -62,6 +75,10 @@ class InformationScreen: UIView {
             //imageHead.centerXAnchor.constraint(equalTo: centerXAnchor),
             imageHead.heightAnchor.constraint(equalToConstant:150),
             imageHead.widthAnchor.constraint(equalToConstant: 750),
+            
+            textLabel.topAnchor.constraint(equalTo: imageHead.bottomAnchor, constant: 50),
+            textLabel.leadingAnchor.constraint(equalTo:titleLabel.leadingAnchor),
+            textLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
             ])
     }
     
