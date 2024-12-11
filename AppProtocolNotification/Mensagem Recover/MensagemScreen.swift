40ba9671245cb39ddc1355e6a6b7428d7247df92
Ronan Soares <soaresronansilva@gmail.com>
2024-12-10 22:02:09 -0300

    tarja de validacao de campo, invocacao de nova tela MessageVC

---
+++ AppProtocolNotification/Mensagem Recover/MensagemScreen.swift
@@ -0,0 +1,20 @@
+//
+//  MensagemScreen.swift
+//  AppProtocolNotification
+//
+//  Created by Ronan Soares on 10/12/24.
+//
+
+import UIKit
+
+class MensagemScreen: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
