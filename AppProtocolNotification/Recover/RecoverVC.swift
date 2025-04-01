3b1bceeb42997c1b8970a75bc9adfd10f6312e93
Ronan Soares <soaresronansilva@gmail.com>
2025-03-31 22:20:02 -0300

    criacao de conta no FIREBASE

--- AppProtocolNotification/Recover/RecoverVC.swift
+++ AppProtocolNotification/Recover/RecoverVC.swift
@@ -10,6 +10,7 @@ import FirebaseAuth
 
 class RecoverVC: UIViewController, UITextFieldDelegate, recoverEmailButtonScreen {
     
+    
     var auth: Auth?  // var do tipo autentificacao
     
     var screen : RecoverScreen?
@@ -26,55 +27,83 @@ class RecoverVC: UIViewController, UITextFieldDelegate, recoverEmailButtonScreen
         auth = Auth.auth() // instancia da var authentic do firebase
         
     }
-   
     
-    func tappedRecoverButtonOne() {
+//    func createAccountFirebase() { // funcao de criacao de conta no FIREBASE
+        // pega os valores contidos nesses campos
+//        let email: String = screen?.emailCreate.text ?? ""
+//        let passwordCreate = screen?.passwordCreate.text ?? ""
+        
+        
+//        auth?.createUser(withEmail: email, password: passwordCreate, completion: { ( result, error)in
+//            if  error != nil {
+//                print("Erro ao criar conta")
+//            }
+//            print("Conta criada com sucesso")
+//        } ) }
+        
+        
+    func tappedRecoverButtonOne() {  // botao que cria a conta no FIREBASE
         let mensage  : MensagemVC = MensagemVC() // instancia a class alvo
         mensage.modalPresentationStyle = .formSheet // customizacao do tamanho de tela
         //present(device, animated: true ,completion: nil) // invoca o método
         present(mensage, animated: true)
-             }
-    
-    func tappedApplelogo() {
-        let logo  : AppleLogoVC = AppleLogoVC() // instancia a class alvo
-        logo.modalPresentationStyle = .formSheet // customizacao do tamanho de tela
-        //present(device, animated: true ,completion: nil) // invoca o método
-        present(logo, animated: true)
-             }
-    
-    func tappedGooglelogo() {
-        let logoView  : GoogleVC = GoogleVC()
-        logoView.modalPresentationStyle = .formSheet
-        present(logoView, animated: true)
-    }
-    
-    
-    
-    
-    // extensao ao protocol
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        print(#function)
-        habilitField()
-    }
-
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        print(#function)
-        screen?.editionButtonrecover() // validacao do campo
-    }
-
-        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        print(#function)
-        textField.resignFirstResponder()// o return do teclado, baixa o teclado
-        return false
-    }
-    
-    func  habilitField () {  // funcao que desabilita advertencia de campo incompleto
-        if screen?.passwordCreate.text?.isEmpty == true{
-            //screen?.textName.layer.borderColor = UIColor.red.cgColor
-            screen?.passwordCreate.layer.borderWidth = 0
+        
+        let email: String = screen?.emailCreate.text ?? ""
+        let password: String = screen?.passwordCreate.text ?? ""
+        
+        
+        auth?.createUser(withEmail: email, password: password) { result, error in
+            if let error = error {
+                print("Erro ao criar conta: \(error.localizedDescription)")
+            } else {
+                print("Conta criada com sucesso")
+            }
         }
-        if screen?.emailCreate.text?.isEmpty == true {
-            //screen?.textPassword.layer.borderColor = UIColor.red.cgColor
-            screen?.emailCreate.layer.borderWidth = 0        }
+
     }
-}
+        
+        func tappedApplelogo() {
+            let logo  : AppleLogoVC = AppleLogoVC() // instancia a class alvo
+            logo.modalPresentationStyle = .formSheet // customizacao do tamanho de tela
+            //present(device, animated: true ,completion: nil) // invoca o método
+            present(logo, animated: true)
+        }
+        
+        func tappedGooglelogo() {
+            let logoView  : GoogleVC = GoogleVC()
+            logoView.modalPresentationStyle = .formSheet
+            present(logoView, animated: true)
+        }
+        
+        
+        
+        
+        // extensao ao protocol
+        func textFieldDidBeginEditing(_ textField: UITextField) {
+            print(#function)
+            habilitField()
+        }
+        
+        func textFieldDidEndEditing(_ textField: UITextField) {
+            print(#function)
+            screen?.editionButtonrecover() // validacao do campo
+        }
+        
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            print(#function)
+            textField.resignFirstResponder()// o return do teclado, baixa o teclado
+            return false
+        }
+        
+        func  habilitField () {  // funcao que desabilita advertencia de campo incompleto
+            if screen?.passwordCreate.text?.isEmpty == true{
+                //screen?.textName.layer.borderColor = UIColor.red.cgColor
+                screen?.passwordCreate.layer.borderWidth = 0
+            }
+            if screen?.emailCreate.text?.isEmpty == true {
+                //screen?.textPassword.layer.borderColor = UIColor.red.cgColor
+                screen?.emailCreate.layer.borderWidth = 0        }
+        }
+    }
+    
+
