4c14f8fba620c763f514b46a163cc288aff1aaf2
Ronan Soares <soaresronansilva@gmail.com>
2025-07-15 17:19:03 -0300

    inserido scrollview na view tipo present

--- AppProtocolNotification/CellClicks/FourtyCellScreen.swift
+++ AppProtocolNotification/CellClicks/FourtyCellScreen.swift
@@ -9,17 +9,22 @@ import UIKit
 
 class FourtyCellScreen: UIView {
 
+    private let scrollView = UIScrollView()
+    private let contentView = UIView()
+
     lazy var TextSecondGenlabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.numberOfLines = 0
         label.font = .systemFont(ofSize: 20, weight: .bold)
         label.textColor = .black
-        label.text = "O novo iMac, com o poderoso chip M4 e a Apple Intelligence, em seu incrível design ultrafino. Graças ao chip M4, o iMac é até 1,7 vez mais rápido em tarefas do dia a dia e até 2,1 vezes mais rápido em processos mais exigentes, como editar fotos ou jogar games, em comparação com o iMac com M11. E, com o Neural Engine no M4, o iMac é o melhor computador tudo em um do mundo com inteligência artificial e foi feito para a Apple Intelligence, o sistema de inteligência pessoal que transforma a maneira de trabalhar, se comunicar e se expressar, sem deixar de proteger a privacidade."
         label.textAlignment = .center
+        label.text = """
+        O novo iMac, com o poderoso chip M4 e a Apple Intelligence, em seu incrível design ultrafino. Graças ao chip M4, o iMac é até 1,7 vez mais rápido em tarefas do dia a dia e até 2,1 vezes mais rápido em processos mais exigentes, como editar fotos ou jogar games, em comparação com o iMac com M11. E, com o Neural Engine no M4, o iMac é o melhor computador tudo em um do mundo com inteligência artificial e foi feito para a Apple Intelligence, o sistema de inteligência pessoal que transforma a maneira de trabalhar, se comunicar e se expressar, sem deixar de proteger a privacidade.
+        """
         return label
     }()
-    
+
     lazy var imagemSecondGen: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -31,28 +36,47 @@ class FourtyCellScreen: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .white
-        addsubviews()
+        setupViews()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    func addsubviews() {
-        addSubview(imagemSecondGen)
-        addSubview(TextSecondGenlabel)
-        
+
+    func setupViews() {
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+
+        addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        contentView.addSubview(imagemSecondGen)
+        contentView.addSubview(TextSecondGenlabel)
+
         NSLayoutConstraint.activate([
-            imagemSecondGen.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 40),
-            imagemSecondGen.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
-            imagemSecondGen.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            // ScrollView ocupa toda a view
+            scrollView.topAnchor.constraint(equalTo: topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: bottomAnchor),
+
+            // contentView dentro da scrollView
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor), // fundamental para rolagem vertical funcionar!
+
+            // imagem
+            imagemSecondGen.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor, constant: 40),
+            imagemSecondGen.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            imagemSecondGen.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
             imagemSecondGen.heightAnchor.constraint(equalToConstant: 220),
-            imagemSecondGen.widthAnchor.constraint(equalToConstant: 220),
-            
+
+            // label
             TextSecondGenlabel.topAnchor.constraint(equalTo: imagemSecondGen.bottomAnchor, constant: 50),
-            TextSecondGenlabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
-            TextSecondGenlabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
-            
-            ])
-            }
+            TextSecondGenlabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            TextSecondGenlabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+            TextSecondGenlabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -40)
+        ])
+    }
 }
