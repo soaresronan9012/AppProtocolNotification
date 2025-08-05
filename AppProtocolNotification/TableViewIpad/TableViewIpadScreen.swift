42b80f6ae65e46e9fa0223e0f8d2281d33b22615
Ronan Soares <soaresronansilva@gmail.com>
2025-08-04 22:15:02 -0300

    criacao tableviewIpad arquivos iniciais

---
+++ AppProtocolNotification/TableViewIpad/TableViewIpadScreen.swift
@@ -0,0 +1,20 @@
+//
+//  TableViewIpadScreen.swift
+//  AppProtocolNotification
+//
+//  Created by Ronan Soares on 04/08/25.
+//
+
+import UIKit
+
+class TableViewIpadScreen: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ AppProtocolNotification/TableViewIpad/TableViewIpadVC.swift
@@ -0,0 +1,29 @@
+//
+//  TableViewIpadVC.swift
+//  AppProtocolNotification
+//
+//  Created by Ronan Soares on 04/08/25.
+//
+
+import UIKit
+
+class TableViewIpadVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
